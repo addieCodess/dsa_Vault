cf56b0847219fab27033b7b655a4a9c075559e0d
addieCodess <rimi.pits@gmail.com>
2025-05-30 21:46:10 +0530

    Add Template

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dna = sc.nextLine();
+        System.out.print(maxRep(dna));
+        sc.close();
+    }
+
+    static int maxRep(String dna) {
+        if (dna == null || dna.length() == 0) return 0;
+
+        char rep = dna.charAt(0);
+        int max = 1;
+        int count = 1;
+
+        for (int i = 1; i < dna.length(); i++) {
+            char now = dna.charAt(i);
+            if (now == rep) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                rep = now;
+                count = 1;
+            }
+        }
+
+        return Math.max(max, count);
+    }
+}
---
+++ Template.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Template {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dna = sc.nextLine();
+        System.out.println(method());
+        sc.close();
+    }
+
+    static void method() {
+
+    }
+}
